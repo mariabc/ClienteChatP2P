11a3ab1c2f8e0a1811994a0d7854522d2e3ab781
maria <maria@192.168.1.64>
2013-04-10 17:03:10 +0200

    Actualizar lista de amigos conectados

--- src/p2pchat/ClienteImpl.java
+++ src/p2pchat/ClienteImpl.java
@@ -4,18 +4,21 @@
  */
 package p2pchat;
 
+import Datos.Amigo;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 
 /**
  *
  * @author maria
  */
-public class ClienteImpl {
+public class ClienteImpl implements ICliente{
     
      private ventana v;
      private IServidor servidor;
      private ICliente cliente;
      private Login chat;
+     private PrincipalChat principal;
 
     
 
@@ -39,6 +42,41 @@ public class ClienteImpl {
          servidor.registro(cliente, nick, nombre, apellido, nick);
              
              }
+
+    @Override
+    public void killMensajeError(String error) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void notificaAmistad(ArrayList<String> Amistad) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void notificarListaAmigos(ArrayList<Amigo> amigo) {
+       principal.actualizaLista(amigo);
+    }
+
+    @Override
+    public void notificarNuevoAmigo(Amigo amigo) {
+        
+        principal.actualizaLista(amigo);
+        
+       
+    }
+
+    @Override
+    public void notificarNuevaAmistad(String nombreAmigo) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void borrarconectado(String nick) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
     
     
 }
